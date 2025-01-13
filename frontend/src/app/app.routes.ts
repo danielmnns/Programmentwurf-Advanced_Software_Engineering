7ee4794caa16402762e0e94e1a955942b666db63
Max Knoblauch <159517121+MaxKnoblauch@users.noreply.github.com>
2025-01-13 13:06:48 +0100

    KursAngelegt

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { AccountComponent } from './account/account.component';  // AccountComponent importieren
+import { KursComponent } from './kurs/kurs.component';  // KursComponent importieren
 
 export const routes: Routes = [
     { path: '', loadComponent: () => import('./login/login.component').then(m => m.LoginComponent)},
     { path: 'account', component: AccountComponent }, // Konto-Seite
+    { path: 'kurs', component: KursComponent },
   ];
 
 @NgModule({
