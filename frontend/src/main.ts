f1de7cad3b0097c5442fa2f63390e9cd53e63af0
Rupprich Marcel <m.rupprich@vrades-maschinenbau.com>
2025-01-16 10:05:41 +0100

    awrfef

--- frontend/src/main.ts
+++
@@ -1,6 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppConfig } from '..src/app/appconfig';
-
-platformBrowserDynamic()
-  .bootstrapModule(AppConfig)
-  .catch(err => console.error(err));
